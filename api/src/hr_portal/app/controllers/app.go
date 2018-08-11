5abb7d94351d1345a8cac41a72ec7f29538d1fc8
ASacanell <alejandro.sacanell@gmail.com>
2018-08-11 17:58:19 +0200

    Allow params in the signup endpoint

--- api/src/hr_portal/app/controllers/app.go
+++ api/src/hr_portal/app/controllers/app.go
@@ -3,13 +3,16 @@ package controllers
 import (
 	"github.com/revel/revel"
 	"hr_portal/app/models"
-
 )
+
 type App struct {
 	*revel.Controller
 }
 
 func (c App) Index() revel.Result {
-	models.CreateUser("Shine")
+	username := c.Params.Form.Get("username")
+	password := c.Params.Form.Get("password")
+
+	models.CreateUser(username, password)
 	return c.Render()
 }
